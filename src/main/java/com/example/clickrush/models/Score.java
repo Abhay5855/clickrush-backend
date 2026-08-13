0a092688ce44750b49471fa4a7333bff22ccaf5a
Abhay5855 <ap3919529@gmail.com>
2026-08-13 20:21:34 +0530

    feat: project level security and local profile

---
+++ src/main/java/com/example/clickrush/models/Score.java
@@ -0,0 +1,6 @@
+package com.example.clickrush.models;
+
+
+
+public class Score {
+}
---
+++ src/main/java/com/example/clickrush/models/User.java
@@ -0,0 +1,24 @@
+package com.example.clickrush.models;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    @Column(name = "password")
+    private String password_hash;
+
+    private LocalDateTime created_at;
+
+}
