558f76b9b14837865c5fa2f1d403cdb507c163e4
Abhay5855 <ap3919529@gmail.com>
2026-08-13 19:24:32 +0530

    feat: project initialisation with folder structure

---
+++ src/main/java/com/example/clickrush/ClickrushApplication.java
@@ -0,0 +1,13 @@
+package com.example.clickrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickrushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickrushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clickrush/ClickrushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clickrush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickrushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
